afa4a0b480bc79731d190a867b24b9718da32b0d
javapagar <apariciogarcia.j@gmail.com>
2021-01-04 11:14:18 +0100

    Implementación de cross origin CORS

--- src/main/java/es/javapagar/spring/backend/controller/ClienteController.java
+++ src/main/java/es/javapagar/spring/backend/controller/ClienteController.java
@@ -3,6 +3,7 @@ package es.javapagar.spring.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.javapagar.spring.backend.model.entity.Cliente;
 import es.javapagar.spring.backend.model.service.IClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("api/clientes")
 public class ClienteController {
